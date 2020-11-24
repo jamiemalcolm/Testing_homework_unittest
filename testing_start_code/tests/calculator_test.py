4da859c76edce1d2dc57840fc9c31563d0191b76
jamiemalcolm <jmalcolm281@gmail.com>
2020-11-24 17:37:54 +0000

    all tests written and passing

---
+++ testing_start_code/tests/calculator_test.py
@@ -0,0 +1,16 @@
+import unittest
+from src.calculator import add, divide, multiply, subtract
+
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(5, add(2, 3))
+
+    def test_subtract(self):
+        self.assertEqual(3, subtract(10, 7))
+
+    def test_divide(self):
+        self.assertEqual(2, divide(8, 4))
+
+    def test_multiply(self):
+        self.assertEqual(25, multiply(5, 5))
